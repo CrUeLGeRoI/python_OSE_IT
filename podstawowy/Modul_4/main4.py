478d71e0bba9192a12a97f45c74f23345cca5476
CrUeLGeRoI <elbg2016@gmail.com>
2022-11-08 18:22:30 +0100

    Modul 4.

---
+++ podstawowy/Modul_4/main4.py
@@ -0,0 +1,11 @@
+a = input("Please enter number: ")  # type = str
+print(a)
+a = int(a)  # type = int
+print(a)
+a = float(a)  # type = float
+print(a)
+print(3 + 3.27586)
+print(8 * 4 + 2)
+a = int(a)  # type = int
+print(a * 9.978)
+print(a ** 4)
